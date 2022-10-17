5a64228306b62461ef7157f340ff25508c8cd207
Ninja J <NMJSN9@users.noreply.github.com>
2022-10-17 15:43:18 +0000

    reason for commit

---
+++ lab_input/challenge01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE 01 - SHEbange, Input , Print, Concatenate, Variables"""
+
+
+def main():
+
+    username_input = input("Your name is: ") 
+
+
+
+    today = input("What is today? '(Mon, Tue ... etc.)': ")
+    ## print() can be given a series of objects separated by a comma
+    print("Hello,", username_input,"! Happy", today," !")
+
+
+main()
+
